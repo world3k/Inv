bbde44d14475bf479e43d8065c4ce8dd3221d5d0
world3k <world3k@gmail.com>
2018-04-10 12:26:12 -0500

    commit

--- src/main/java/com/world/inv/utils/UserRepository.java
+++ src/main/java/com/world/inv/utils/UserRepository.java
@@ -3,7 +3,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.world.inv.entities.User;
 //This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-//CRUD refers Create, Read, Update, Delete
+//CRUD refers Create, Read, Update, Delete  
+/// PagingAndSortingRepository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
