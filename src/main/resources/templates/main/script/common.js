87f25fc059a1851229a0fde312aad730d6b7efb9
Your Name <you@example.com>
2018-04-11 23:03:05 +0800

    update

--- src/main/resources/templates/main/script/common.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.easyui-switchbutton').switchbutton({
-        width: 80,
-        onChange: function (value) {
-            dump({values: value, value: '22222'});
-        }
-    });
-});
